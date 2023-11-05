72b480bf08891aa5d733190501b290296fc3287c
vizels <mirchuk.georgiy@gmail.com>
2023-11-05 15:32:31 +0100

    Fixed working init version

--- adaptive_regulation_proj1.py
+++ adaptive_regulation_proj1.py
@@ -3,25 +3,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-
-class Sawtooth:
-    def __init__(self, samples):
-            self.t = np.linspace(0, 2*np.pi, samples) #time
-            self.y = signal.sawtooth(self.t, 0.5) #triangle wave
-
 class Measure:
-    def __init__(self, t, y, noise_spread = 0.2):
-        self.t = t
-        self.y = []
-        for i in y:
-            noise = ( random.random() - 0.5 ) * noise_spread
-            self.y.append(i + noise) 
+    def __init__(self, cycles, signal_samples, measure_samples, noise_spread = 0.2):
+        self.t_signal = np.linspace(0, cycles*2*np.pi, signal_samples) # create time axis for original signal
+        self.y_signal = signal.sawtooth(self.t_signal, 0.5) # y - triangle original function 
+
+        self.t_measure = np.linspace(0, cycles*2*np.pi, measure_samples) # create time axis for measurement
+        self.y_measure = []
+
+        #iterate over original signal and add some noise
+        for i in range(0, len(self.y_signal), signal_samples//measure_samples):
+            noise = ( random.random() - 0.5 ) * noise_spread 
+            self.y_measure.append(self.y_signal[i] + noise) 
         
-fala = Sawtooth()
-pomiar = Measure(fala.t, fala.y)
+pomiar = Measure(3, 250, 125)
 
-plt.plot(fala.t, fala.y)
+plt.plot(pomiar.t_signal, pomiar.y_signal)
 
 
-plt.plot(pomiar.t, pomiar.y, '.')
+plt.plot(pomiar.t_measure, pomiar.y_measure, '.')
 plt.show()
