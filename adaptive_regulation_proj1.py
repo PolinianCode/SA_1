53b65e1f3f6a0da2d428ecc9e0ba1cef35b5826e
vizels <mirchuk.georgiy@gmail.com>
2023-11-05 21:23:04 +0100

    Bad working MSE

--- adaptive_regulation_proj1.py
+++ adaptive_regulation_proj1.py
@@ -7,7 +7,8 @@ def MSE(original, data):
     N = len(original)
     sum = 0
     for i in range(0, N):
-        sum += (original[i]-data[i])**2
+        if data[i] is not None:
+            sum += (original[i]-data[i])**2
     
     result = sum/N
     
@@ -30,11 +31,18 @@ class Measure:
     def filter_signal(self, H):
         self.H = H
 
-        self.y_filtered = np.convolve(self.y_measure, np.ones(H) / H, mode='valid')
+        self.y_filtered = [None] * len(self.y_measure)
+        self.filtered_data = []
 
-        start_index = H // 2
-        end_index = start_index + len(self.y_filtered)
-        self.t_filtered = pomiar.t_measure[start_index:end_index]
+        for i in range(0, len(self.y_measure)-H):
+            step_mean = (np.sum(self.y_measure[i:i+H]))/H
+            self.filtered_data.append(step_mean)
+        
+        filtered_start_point = (H//2)
+        filtered_end_point = -((H//2)+(H%2))
+        self.y_filtered[filtered_start_point:filtered_end_point] = self.filtered_data[:]
+
+        self.t_filtered = pomiar.t_measure
     
     def plots(self, *args):
         plot_options = {'signal': (self.t_signal, self.y_signal, 'b', 'Original Signal'),
@@ -49,10 +57,24 @@ class Measure:
         plt.show()
 
         
+signal_samples = 1000
+measure_samples = 250
 
-pomiar = Measure(3, 1000, 250, noise_spread=0.5)
 
-pomiar.filter_signal(7)
-
+pomiar = Measure(3, signal_samples, measure_samples, noise_spread=0)
+pomiar.filter_signal(1)
 pomiar.plots('signal', 'measure', 'filtered')
+
+MSE_table = []
+
+# for i in range(1, 21):
+#     pomiar = Measure(3, signal_samples, measure_samples, noise_spread=0.5)
+#     pomiar.filter_signal(i)
+#     MSE_table.append(MSE(pomiar.y_signal[2::(signal_samples//measure_samples)], pomiar.y_filtered))
+
+#x = np.linspace(1,20,20, dtype=np.int32)
+#plt.plot(x, MSE_table)
+plt.show()
+
+
 
