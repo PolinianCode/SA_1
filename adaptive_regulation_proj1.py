32e17bea844f91ba86707672c788e42d872709cb
Danylo <danilkoval2004@gmail.com>
2023-11-05 16:12:53 +0100

    Added moving average func

--- adaptive_regulation_proj1.py
+++ adaptive_regulation_proj1.py
@@ -4,22 +4,27 @@ import numpy as np
 import random
 
 class Measure:
-    def __init__(self, cycles, signal_samples, measure_samples, noise_spread = 0.2):
-        self.t_signal = np.linspace(0, cycles*2*np.pi, signal_samples) # create time axis for original signal
-        self.y_signal = signal.sawtooth(self.t_signal, 0.5) # y - triangle original function 
+    def __init__(self, cycles, signal_samples, measure_samples, noise_spread=0.3):
+        self.t_signal = np.linspace(0, cycles * 2 * np.pi, signal_samples)
+        self.y_signal = signal.sawtooth(self.t_signal, 0.5)
 
-        self.t_measure = np.linspace(0, cycles*2*np.pi, measure_samples) # create time axis for measurement
+        self.t_measure = np.linspace(0, cycles * 2 * np.pi, measure_samples)
         self.y_measure = []
 
-        #iterate over original signal and add some noise
-        for i in range(0, len(self.y_signal), signal_samples//measure_samples):
-            noise = ( random.random() - 0.5 ) * noise_spread 
-            self.y_measure.append(self.y_signal[i] + noise) 
-        
+        for i in range(0, len(self.y_signal), signal_samples // measure_samples):
+            noise = (random.random() - 0.5) * noise_spread
+            self.y_measure.append(self.y_signal[i] + noise)
+
+h = 5  
 pomiar = Measure(3, 250, 125)
 
-plt.plot(pomiar.t_signal, pomiar.y_signal)
+def sredniaRuchoma(probki, h):
+    return np.convolve(probki, np.ones(h) / h, mode='same')[:len(probki)]
 
+filtered_signal = sredniaRuchoma(pomiar.y_measure, h)
 
-plt.plot(pomiar.t_measure, pomiar.y_measure, '.')
+plt.plot(pomiar.t_signal, pomiar.y_signal, label='Original Signal')
+#plt.plot(pomiar.t_measure, pomiar.y_measure, '.', label='Noisy Measured Signal')
+plt.plot(pomiar.t_measure, filtered_signal, 'g--', label='Filtered Signal')
+plt.legend()
 plt.show()
