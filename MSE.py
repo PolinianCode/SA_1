e090b560f7cc176f03e1445c00399d78133b3757
vizels <mirchuk.georgiy@gmail.com>
2023-11-19 20:19:52 +0100

    Final version

--- MSE.py
+++ MSE.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import Measure
+import measure
 
 def MSE(original, data):
     N = len(original)
@@ -17,7 +17,7 @@ def MSE(original, data):
 def H_plot(signal_samples, measure_samples):
     MSE_table = []
 
-    pomiar = Measure.Measure(3, signal_samples, measure_samples, noise_spread=0.5)
+    pomiar = measure.Measure(3, signal_samples, measure_samples, noise_spread=0.5)
 
     h_range = 13
     for i in range(1, h_range*2, 2):
@@ -42,7 +42,7 @@ def noise_variance_plot(signal_samples, measure_samples, H):
     MSE_table = []
     for i in range(0, 9):
         noise_spread = i/10
-        pomiar = Measure.Measure(3, signal_samples, measure_samples, noise_spread)
+        pomiar = measure.Measure(3, signal_samples, measure_samples, noise_spread)
         pomiar.filter_signal(H)
         MSE_table.append(MSE(pomiar.original_y[::(signal_samples//measure_samples)], pomiar.filtered_y))
 
@@ -56,25 +56,32 @@ def noise_variance_plot(signal_samples, measure_samples, H):
     plt.show()
 
 
-def h_optymalne_dla_varz(signal_samples, measure_samples):
+def h_optymalne_dla_varz(signal_samples, measure_samples, max_noise_spread_100x, noise_spread_step, max_H):
 
     H_opt_table = []
     VarZ_table = []
 
-    for i in range(0, 30):
-        MSE_table = []
-        noise_spread = i/10
+    for i in range(0, max_noise_spread_100x, noise_spread_step):
+        noise_spread = i/100
         VarZ_table.append((noise_spread ** 2) / 3)
-        for j in range(1, 30):
-            pomiar = Measure.Measure(3, signal_samples, measure_samples, noise_spread)
-            pomiar.filter_signal(j)
-            MSE_table.append(MSE(pomiar.original_y[::(signal_samples//measure_samples)], pomiar.filtered_y))
 
-        H_opt_table.append(np.argmin(MSE_table) + 1)
+        H_sub_optimal_table = []    
 
-    
+        for _ in range(20):    
+            MSE_table = []
+            for j in range(1, max_H):
+                pomiar = measure.Measure(3, signal_samples, measure_samples, noise_spread)
+                pomiar.filter_signal(j)
+                MSE_table.append(MSE(pomiar.original_y[::(signal_samples//measure_samples)], pomiar.filtered_y))
+
+            H_opt = np.argmin(MSE_table) + 1
+            H_sub_optimal_table.append(H_opt)
+
+        H_opt_table.append(np.mean(H_sub_optimal_table))
+
+
     plt.figure()
-    plt.plot(VarZ_table, H_opt_table, marker='o', linestyle='-')
+    plt.plot(VarZ_table, H_opt_table, 'go-')
     plt.ylabel("H optymalne")
     plt.xlabel("Var(Z)")
 
--- adaptive_regulation_proj1.py
+++ adaptive_regulation_proj1.py
@@ -1,20 +1,19 @@
-import Measure
+import measure
 import MSE
 
 ORIGINAL_SAMPLES_NUM = 1000
 MEASURED_SAMPLES_NUM = 250
 
+pomiar = measure.Measure(3, ORIGINAL_SAMPLES_NUM, MEASURED_SAMPLES_NUM, noise_spread=0.3)
+pomiar.filter_signal(3)
+pomiar.plots('signal', 'measured', 'filtered')
+pomiar.plots('signal', 'filtered')
 
 MSE.noise_variance_plot(ORIGINAL_SAMPLES_NUM, MEASURED_SAMPLES_NUM, 9)
 
-#MSE.H_plot(ORIGINAL_SAMPLES_NUM, MEASURED_SAMPLES_NUM)
+MSE.H_plot(ORIGINAL_SAMPLES_NUM, MEASURED_SAMPLES_NUM)
 
-pomiar = Measure.Measure(3, ORIGINAL_SAMPLES_NUM, MEASURED_SAMPLES_NUM, noise_spread=0.3)
-
-MSE.h_optymalne_dla_varz(ORIGINAL_SAMPLES_NUM, MEASURED_SAMPLES_NUM)
-#pomiar.filter_signal(3)
-#pomiar.plots('signal', 'measured', 'filtered')
-#pomiar.plots('signal', 'filtered')
+MSE.h_optymalne_dla_varz(ORIGINAL_SAMPLES_NUM, MEASURED_SAMPLES_NUM, 100, 10, 30)
 
 
 
